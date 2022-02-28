7d0f45120c6f3096d07146dc74344365f4f3027c
German <germanmarpar21@gmail.com>
2022-02-27 21:36:06 -0300

    second commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 const app = express();
 
 //Configure dotenv
